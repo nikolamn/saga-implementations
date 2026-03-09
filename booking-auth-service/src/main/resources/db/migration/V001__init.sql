683ea9a07bdfcb7abb089cfb7f060476b3d832ff
nikolamn <user6666user7777@gmail.com>
2026-03-09 19:15:08 +0100

    basic signup within authservice; migrations added; tests

--- booking-auth-service/src/main/resources/db/migration/V001__init.sql
+++ booking-auth-service/src/main/resources/db/migration/V001__init.sql
@@ -6,9 +6,9 @@ BEGIN
 END$$;
 
 CREATE TABLE IF NOT EXISTS auth_user (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    username VARCHAR(20) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    id UUID PRIMARY KEY,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     role ROLE NOT NULL,
     CONSTRAINT username_length_check CHECK (char_length(username) BETWEEN 12 AND 20),
     CONSTRAINT username_unique_check UNIQUE (username)
